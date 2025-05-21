31986784aa99f6e5c73347818b3adf6437cc2c8c
ikuko <magi.ikuko@gmail.com>
2025-05-21 17:07:34 +0900

    refactor: 内部処理最適化

--- Packages/com.hoshinolabs.sardinal/Runtime/Udon/SardinalShim.cs
+++ Packages/com.hoshinolabs.sardinal/Runtime/Udon/SardinalShim.cs
@@ -13,27 +13,21 @@ namespace HoshinoLabs.Sardinal.Udon {
         [Inject, SerializeField, HideInInspector]
         string[] _1;
         [Inject, SerializeField, HideInInspector]
-        int[] _2;
+        long[] _2;
         [Inject, SerializeField, HideInInspector]
-        object[][] _3;
+        string[] _3;
         [Inject, SerializeField, HideInInspector]
-        IUdonEventReceiver[][] _4;
+        string[][] _4;
         [Inject, SerializeField, HideInInspector]
-        int[][] _5;
+        string[][] _5;
         [Inject, SerializeField, HideInInspector]
-        int _6;
+        bool[] _6;
         [Inject, SerializeField, HideInInspector]
-        string[] _7;
+        int[] _7;
         [Inject, SerializeField, HideInInspector]
-        long[] _8;
+        object[][] _8;
         [Inject, SerializeField, HideInInspector]
-        string[] _9;
-        [Inject, SerializeField, HideInInspector]
-        string[][] _10;
-        [Inject, SerializeField, HideInInspector]
-        string[][] _11;
-        [Inject, SerializeField, HideInInspector]
-        bool[] _12;
+        IUdonEventReceiver[][] _9;
 
         internal void Publish(string topic, object channel, params object[] args) {
             var argsLength = args.Length;
@@ -45,116 +39,83 @@ namespace HoshinoLabs.Sardinal.Udon {
             if (idx < 0) {
                 return;
             }
-            var __2 = _2[idx];
             var __3 = _3[idx];
             var __4 = _4[idx];
-            var __5 = _5[idx];
-            for (var i = 0; i < __2; i++) {
-                if (channel != null && __3[i] != null && !__3[i].Equals(channel)) {
+            var __7 = _7[idx];
+            var __8 = _8[idx];
+            var __9 = _9[idx];
+            for (var i = 0; i < __7; i++) {
+                var ___8 = __8[i];
+                if (channel != null && ___8 != null && !___8.Equals(channel)) {
                     continue;
                 }
-                var ___4 = __4[i];
-                var ___5 = __5[i];
-                var __10 = _10[___5];
+                var ___9 = __9[i];
                 for (var j = 0; j < argsLength; j++) {
-                    ___4.SetProgramVariable(__10[j], args[j]);
+                    ___9.SetProgramVariable(__4[j], args[j]);
                 }
-                ___4.SendCustomEvent(_9[___5]);
+                ___9.SendCustomEvent(__3);
             }
         }
 
         internal void Subscribe(string topic, object channel, UdonSharpBehaviour subscriber) {
             var udonId = subscriber.GetUdonTypeID();
             var receiver = (IUdonEventReceiver)(object)subscriber;
-            for (var i = 0; i < _6; i++) {
-                var __7 = _7[i];
-                if (!__7.StartsWith(topic) || _8[i] != udonId) {
+            for (var i = 0; i < _0; i++) {
+                if (_2[i] != udonId) {
                     continue;
                 }
-                var idx = Array.IndexOf(_1, __7);
-                if (idx < 0) {
-                    var __0a = _0;
-                    _0 = __0a + 1;
-                    var __1a = new string[_0];
-                    Array.Copy(_1, __1a, __0a);
-                    __1a[__0a] = __7;
-                    _1 = __1a;
-                    var __2a = new int[_0];
-                    Array.Copy(_2, __2a, __0a);
-                    __2a[__0a] = 1;
-                    _2 = __2a;
-                    var __3a = new object[_0][];
-                    Array.Copy(_3, __3a, __0a);
-                    __3a[__0a] = new[] { channel };
-                    _3 = __3a;
-                    var __4a = new IUdonEventReceiver[_0][];
-                    Array.Copy(_4, __4a, __0a);
-                    __4a[__0a] = new[] { receiver };
-                    _4 = __4a;
-                    var __5a = new int[_0][];
-                    Array.Copy(_5, __5a, __0a);
-                    __5a[__0a] = new[] { i };
-                    _5 = __5a;
+                var __1 = _1[i];
+                if (!__1.StartsWith(topic)) {
+                    continue;
                 }
-                else {
-                    if (0 <= Array.IndexOf(_4[idx], receiver)) {
-                        continue;
-                    }
-                    var __2a = _2[idx];
-                    var __2b = __2a + 1;
-                    _2[idx] = __2b;
-                    var __3a = new object[__2b];
-                    Array.Copy(_3[idx], __3a, __2a);
-                    __3a[__2a] = channel;
-                    _3[idx] = __3a;
-                    var __4a = new IUdonEventReceiver[__2b];
-                    Array.Copy(_4[idx], __4a, __2a);
-                    __4a[__2a] = receiver;
-                    _4[idx] = __4a;
-                    var __5a = new int[__2b];
-                    Array.Copy(_5[idx], __5a, __2a);
-                    __5a[__2a] = i;
-                    _5[idx] = __5a;
+                if (0 <= Array.IndexOf(_9[i], receiver)) {
+                    continue;
                 }
+                var __7a = _7[i];
+                var __7b = __7a + 1;
+                _7[i] = __7b;
+                var __8a = new object[__7b];
+                Array.Copy(_8[i], __8a, __7a);
+                __8a[__7a] = channel;
+                _8[i] = __8a;
+                var __9a = new IUdonEventReceiver[__7b];
+                Array.Copy(_9[i], __9a, __7a);
+                __9a[__7a] = receiver;
+                _9[i] = __9a;
             }
         }
 
         internal void Unsubscribe(string topic, UdonSharpBehaviour subscriber) {
             var udonId = subscriber.GetUdonTypeID();
             var receiver = (IUdonEventReceiver)(object)subscriber;
-            for (var i = 0; i < _6; i++) {
-                var __7 = _7[i];
-                if (!__7.StartsWith(topic) || _8[i] != udonId) {
+            for (var i = 0; i < _0; i++) {
+                if (_2[i] != udonId) {
                     continue;
                 }
-                var idx = Array.IndexOf(_1, __7);
-                if (idx < 0) {
+                var __1 = _1[i];
+                if (!__1.StartsWith(topic)) {
                     continue;
                 }
-                var __4a = _4[idx];
-                var dest = Array.IndexOf(__4a, receiver);
+                var __9a = _9[i];
+                var dest = Array.IndexOf(__9a, receiver);
                 if (dest < 0) {
                     continue;
                 }
-                var __2a = _2[idx];
-                var __2b = __2a - 1;
-                _2[idx] = __2b;
+
+                var __7a = _7[i];
+                var __7b = __7a - 1;
+                _7[i] = __7b;
                 var src = dest + 1;
-                var length = __2a - src;
-                var __3a = _3[idx];
-                var __3b = new object[__2b];
-                Array.Copy(__3a, 0, __3b, 0, dest);
-                Array.Copy(__3a, src, __3b, dest, length);
-                _3[idx] = __3b;
-                var __4b = new IUdonEventReceiver[__2b];
-                Array.Copy(__4a, 0, __4b, 0, dest);
-                Array.Copy(__4a, src, __4b, dest, length);
-                _4[idx] = __4b;
-                var __5a = _5[idx];
-                var __5b = new int[__2b];
-                Array.Copy(__5a, 0, __5b, 0, dest);
-                Array.Copy(__5a, src, __5b, dest, length);
-                _5[idx] = __5b;
+                var length = __7a - src;
+                var __8a = _8[i];
+                var __8b = new object[__7b];
+                Array.Copy(__8a, 0, __8b, 0, dest);
+                Array.Copy(__8a, src, __8b, dest, length);
+                _8[i] = __8b;
+                var __9b = new IUdonEventReceiver[__7b];
+                Array.Copy(__9a, 0, __9b, 0, dest);
+                Array.Copy(__9a, src, __9b, dest, length);
+                _9[i] = __9b;
             }
         }
     }
