584604ca1394b28717e63cddd7db5dcb7de7705d
ikuko <magi.ikuko@gmail.com>
2025-06-23 13:39:38 +0900

    refactor: クラス名修正

--- Packages/com.hoshinolabs.sardinal/Editor/Udon/ContainerBuilderExtensions.cs
+++ Packages/com.hoshinolabs.sardinal/Editor/Udon/ContainerBuilderExtensions.cs
@@ -1,7 +1,7 @@
 using HoshinoLabs.Sardinject;
 
 namespace HoshinoLabs.Sardinal.Udon {
-    internal static class ComponentContainerBuilderExtensions {
+    internal static class ContainerBuilderExtensions {
         public static ComponentBindingBuilder OverrideSardinalInjection(this ContainerBuilder self) {
             var destination = new ComponentDestination();
             var resolverBuilder = new SardinalResolverBuilder(destination).OverrideScopeIfNeeded(self, Lifetime.Cached);
